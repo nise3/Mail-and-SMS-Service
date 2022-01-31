20857a77b75854ffa0cd0859a8115f8a2c6cea92
ASM Mahmudul Hasan <asmmahmud@gmail.com>
2022-01-31 13:37:24 +0600

    New Release: 1.0.5

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', 3306),
-            'database' => env('DB_DATABASE', 'mail'),
+            'database' => env('DB_DATABASE', 'nise_mail_sms'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
