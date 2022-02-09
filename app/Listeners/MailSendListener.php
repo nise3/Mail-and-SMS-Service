4f2f0c8a58df6803abc28bde0cc70f83866df927
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2022-02-09 22:12:29 +0600

    Mail log commented

--- app/Listeners/MailSendListener.php
+++ app/Listeners/MailSendListener.php
@@ -26,10 +26,10 @@ class MailSendListener implements ShouldQueue
         $mailData = $mailData['data'] ?? [];
 
         if (!empty($mailData) && !empty($mailData['to']) && !empty($mailData['from']) && !empty($mailData['subject']) && !empty($mailData['message_body'])) {
-            Log::channel('mail_log')->info('Mail Sent to: ' . $mailData['to'] . " - " . $mailData['from'] . ' - ' . $mailData['subject']);
+//            Log::channel('mail_log')->info('Mail Sent to: ' . $mailData['to'] . " - " . $mailData['from'] . ' - ' . $mailData['subject']);
             $this->mailService->sendMail($mailData);
         } else {
-            Log::channel('mail_log')->info("Mail payload format is invalid & the payload is " . json_encode($mailData));
+//            Log::channel('mail_log')->info("Mail payload format is invalid & the payload is " . json_encode($mailData));
             throw_if(true, Exception::class, "Mail payload format is invalid   & the payload is ");
         }
     }
